83634cde24dcbd3e7d571189da5c4f8edcd9313b
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-09-05 12:24:59 +0530

    Linking JS , Variables

---
+++ README.md
@@ -0,0 +1,4 @@
+Learning Complete Javascript from Basics
+
+1 - Linking Javascript into HTML.
+2 - Variables , Variable declarations.
---
+++ script.js
@@ -0,0 +1,24 @@
+let js = 'amazing';
+
+if (js === 'amazing') alert('Javascript is Fun!');
+
+console.log(40 + 8 + 65 + 15 - 91);
+
+console.log('Saurav');
+
+let firstName = 'Saurav';
+console.log(firstName);
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
+
+let country = 'India';
+let continent = 'Asia';
+let population = 1393.4;
+
+console.log(country);
+console.log(continent);
+console.log(population);
