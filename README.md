839e2ed3e9d7347030019b60b9536afb4ec3431a
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-09-05 12:49:22 +0530

    Variables, Datatypes, let, const var

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ Learning Complete Javascript from Basics
 
 1 - Linking Javascript into HTML.
 2 - Variables , Variable declarations.
+
+3 - Datatypes
+
+a - Numbers - Floating Point Numbers - ( Used for Decimals and Integers).
+
+b - String - Sequence of Characters - (Used for Text)
+c - Boolean - Logical Type that can only be a true or false - ( Used for Taking Desicions )
+
+d - Undefined - Value taken by a variable that is not yet defined - ( Empty Values )
+
+e - Null - Also Means 'Empty Value'
+
+f - Symbol (ES2015) - Value that is Unique and cannot be changed.
+
+g - BigInt(ES2020) - Larger integers than the Number type can hold.
+
+4 - Comments in JS
+
+5 - let Vs const vs var
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let js = 'amazing';
 
-if (js === 'amazing') alert('Javascript is Fun!');
+// if (js === 'amazing') alert('Javascript is Fun!');
 
 console.log(40 + 8 + 65 + 15 - 91);
 
@@ -22,3 +22,32 @@ let population = 1393.4;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+
+// let for the variables that might change
+
+let age = 23;
+age = 24;
+console.log(age);
+
+let birthyear;
+birthyear = 1998;
+console.log(birthyear);
+
+// const where the values will never change
+// we can not initialize variable without value using const
+
+const PI = 3.14151;
+console.log(PI);
+
+// const YEAR;
+// YEAR = 2022;
+// Not Possible
+
+//
+
+var teacher = 'Science';
+teacher = 'Maths';
